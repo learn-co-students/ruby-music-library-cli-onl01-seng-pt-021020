636d77742b51633b79f4e9c3cba72ef37cc70d6a
Amber Williams <awilliams1234@live.com>
2020-03-21 21:05:18 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,9 +1,16 @@
 class Genre 
   
+  @@all = []
+  
   attr_accessor :name 
   
   def initialize(name)
     @name = name
+    @@all << self
+  end 
+  
+  def self.all
+    @@all
   end 
   
 end 
--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,14 @@
 class Song 
+ @@all = []
   
   attr_accessor :name 
   
   def initialize(name)
     @name = name
-    
+    @@all << self
   end 
   
+  def self.all
+    @@all
+  end 
 end 
