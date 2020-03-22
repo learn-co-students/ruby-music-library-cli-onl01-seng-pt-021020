582ac9e3135fec8ae3db0624f5589d699ef0f4d1
jkatem <jenmiao@gmail.com>
2020-03-22 16:04:40 -0700

    Done.

--- lib/MusicImporter.rb
+++ lib/MusicImporter.rb
@@ -10,5 +10,9 @@ class MusicImporter
     def files
         Dir.entries(@path).select {|x| x.include?(".mp3")}
     end
+
+    def import 
+        self.files.each {|y| Song.create_from_filename(y)}
+    end
   
 end
