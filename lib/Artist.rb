d47ff7544293158e5d6bb84006099c72da776567
jkatem <jenmiao@gmail.com>
2020-03-21 20:36:13 -0700

    Done.

---
+++ lib/Artist.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Artist
+
+    attr_accessor :name 
+
+    @@all = []
+
+    def initialize(artist_name)
+        @name = artist_name
+    end   
+    
+    def self.all 
+        @@all
+    end
+
+    def self.destroy_all
+        @@all.clear 
+    end
+
+    def save
+        @@all << self
+    end 
+
+    def self.create(artist_name)
+        created_artist = Artist.new(artist_name)
+        created_artist.save
+        created_artist
+    end
+    
+end
---
+++ lib/Song.rb
@@ -0,0 +1,31 @@
+require 'pry'
+
+class Song
+
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        
+    end
+
+    def self.all
+        @@all 
+    end
+
+    def self.destroy_all
+        @@all.clear
+    end
+
+    def save
+        @@all << self 
+    end
+
+    def self.create(song_name)
+        created_song = Song.new(song_name)
+        created_song.save
+        created_song
+    end
+end
