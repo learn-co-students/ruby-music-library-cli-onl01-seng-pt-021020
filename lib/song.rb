352f389fa3dee1904eca16d6715a4856da808f78
Amber Williams <awilliams1234@live.com>
2020-04-05 19:31:43 +0000

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -34,11 +34,11 @@ class Song
   song
   end
 
-  def find_by_name(name)
+  def self.find_by_name(name)
     @@all.find{|song| song.name == name}
   end
 
-  def find_or_create
+  def self.find_or_create
 
   end
 
