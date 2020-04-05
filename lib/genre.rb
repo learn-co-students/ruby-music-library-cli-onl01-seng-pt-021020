923f01c81a3043bd83ff82562f89751f82c666d7
Amber Williams <awilliams1234@live.com>
2020-04-05 19:11:31 +0000

    Done.

--- lib/genre.rb
+++ lib/genre.rb
@@ -1,29 +1,30 @@
-class Genre 
-  
+class Genre
+
   @@all = []
-  
-  attr_accessor :name 
-  
+
+  attr_accessor :name, :songs
+
   def initialize(name)
     @name = name
+    @songs = []
     @@all << self
-  end 
-  
+  end
+
   def self.all
     @@all
-  end 
-  
-  def save 
-    @@all << self 
-  end 
-  
+  end
+
+  def save
+    @@all << self
+  end
+
   def self.destroy_all
     @@all.clear
-  end 
-  
+  end
+
   def self.create(song)
   genre = Genre.new(song)
   self.all << self
-  genre 
-  end 
-end 
+  genre
+  end
+end
