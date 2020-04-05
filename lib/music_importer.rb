7bd9e9fbe2f708ccc18d016a7f5a8ddfb0ebaa9c
Amber Williams <awilliams1234@live.com>
2020-04-05 23:13:23 +0000

    Done.

--- lib/music_importer.rb
+++ lib/music_importer.rb
@@ -1,5 +1,7 @@
 class MusicImporter
 
+  attr_accessor :path, :song, :artist, :genre, :musiclibrarycontroller
+
   def initialize(path)
     @path = path
   end
--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -1,9 +1,11 @@
 class MusicLibraryController
 
-  attr_accessor :paths
+  attr_accessor :path
 
   def initialize(path = "./db/mp3s")
     MusicImporter.new(path).import
   end
 
+  def call
+  end
 end
