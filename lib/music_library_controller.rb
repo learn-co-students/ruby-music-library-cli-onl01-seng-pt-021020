d8e1008044c972d60cee14722d40f96d54d29205
Amber Williams <awilliams1234@live.com>
2020-04-05 23:44:10 +0000

    Done.

--- lib/music_library_controller.rb
+++ lib/music_library_controller.rb
@@ -34,4 +34,25 @@ class MusicLibraryController
       end
     end
   end
+
+  def list_songs
+  end
+
+  def list_artists
+  end
+
+  def list_genres
+  end
+
+  def list_songs_by_artist
+  end
+
+  def list_songs_by_genre
+  end
+
+  def play_song
+  end
+
+
+
 end
